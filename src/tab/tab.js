88e62f92ef9dc236bbd421cc8442a8404efe18d6
kang <kjxstar@gmail.com>
2020-03-28 10:53:41 +0800

    add element

--- src/tab/tab.js
+++ src/tab/tab.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App';
 import store from '../store';
 import router from './router';
+import Element from 'element-ui';
+
+Vue.use(Element, { size: 'small', zIndex: 3000 });
 
 global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
